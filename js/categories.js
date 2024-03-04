512557412c35f2355d9d670b66d1d314f1c0306a
mariagerotti <mariaeduarda.gerotti@gmail.com>
2024-03-04 07:49:41 -0300

    fix

--- js/categories.js
+++ js/categories.js
@@ -1,86 +1,137 @@
-const getLocalStorage = () =>
-  JSON.parse(localStorage.getItem("db_categories")) ?? [];
+const apiUrl = "http://localhost/routes/category.php";
 
-const setLocalStorage = (dbCategories) =>
-  localStorage.setItem("db_categories", JSON.stringify(dbCategories));
+const contactForm = document.getElementById("inputsCategories");
+const contactTable = document.getElementById("tbodyCategories");
 
-const createCategory = (categories) => {
-  const dbCategory = getLocalStorage();
-  dbCategory.push(categories);
-  setLocalStorage(dbCategory);
-  //code akki
-  //const data = new formdata(form)
-};
+function postarCateg() {
+  contactForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const data = new FormData(contactForm);
+    try {
+      const res = await fetch(
+        "http://localhost/routes/category.php",
+        {
+          method: "POST",
+          body: data,
+        },
+        window.location.reload()
+      );
+    } catch (error) {
+      console.log(error.message);
+    }
+  });
+}
 
-//const form = document.getElementById('//id do form')
+async function mostrarCateg() {
+  const listaCat = document.getElementById("tbodyCategories");
+  const response = await fetch(apiUrl);
+  const categoriesList = await response.json();
+  listaCat.innerHTML = "";
 
-const isValidField = () => {
-  return document.getElementById("inputsCategories").reportValidity();
-};
+  categoriesList.forEach((categories) => {
+    const tr = document.createElement("tr");
+    tr.innerHTML = `<tr>
+      <td>${categories.code}</td>
+      <td>${categories.name}</td>
+      <td>${categories.tax}</td>
+      <td><button onclick="deleteCategory(${categories.code})">Delete</td>
+      </tr>`;
+    listaCat.appendChild(tr);
+  });
+}
 
-const clearFields = () => {
-  const fields = document.getElementById("nameCategories", "taxCategories");
-  fields.foreEach((field) => (field.value = ""));
-};
-//interação com layout
-const saveCategory = () => {
-  if (isValidField()) {
-    const categories = {
-      id: Math.random().toString(36).substr(2, 6),
-      nameCategories: document.getElementById("nameCategories").value.replace(/</g,
-      "&lt;"
-      ).replace(/>/g,
-      "&gt;"
-      ),
-      taxCategories: document.getElementById("taxCategories").value.replace(/</g,
-      "&lt;"
-      ).replace(/>/g,
-      "&gt;"
-      ),
-    };
-    createCategory(categories);
-  }
-};
+mostrarCateg()
 
-const createRow = (categories) => {
-  const newRow = document.createElement("tr");
-  newRow.innerHTML = `
-  <td>${categories.id}</td>
-  <td>${categories.nameCategories}</td>
-  <td>${categories.taxCategories}</td>
-  <td><button onclick="deleteCategory(${categories.id})">Delete</td>
-  `;
-  document.getElementById("tableCategories").appendChild(newRow);
-};
+async function showAddedCat() {
+  const response = await fetch(apiUrl);
+  const categoriesList = await response.json();
+  categoriesList.forEach((categories) => {
+    const tr = document.createElement("tr");
+    tr.innerHTML = `<tr>
+      <td>${categories.code}</td>
+      <td>${categories.name}</td>
+      <td>${categories.tax}</td>
+      <td><button onclick="deleteCategory(${categories.code})">Delete</td>
+      </tr>`;
+    listaCat.appendChild(tr);
+  });
+  console.log(showAddedCat);
+}
 
-const clearTable = () => {
-  const rows = document.querySelectorAll("#tableCategories>tbody tr");
-  rows.forEach((row) => row.parentNode.removeChild(row));
-};
+// const getLocalStorage = () =>
+// JSON.parse(localStorage.getItem("db_categories")) ?? [];
 
-const updateTable = () => {
-  const dbCategory = getLocalStorage();
-  clearTable();
-  dbCategory.forEach(createRow);
-};
+// const setLocalStorage = (dbCategories) =>
+// localStorage.setItem("db_categories", JSON.stringify(dbCategories));
 
-const fillFields = (categories) => {
-  document.getElementById("nameCategories").value = categories.nameCategories;
-  document.getElementById("taxCategories").value = categories.taxCategories;
-  document.getElementById("nameCategories").dataset.index = categories.index;
-};
+// const createCategory = (categories) => {
+//   const dbCategory = getLocalStorage();
+//   dbCategory.push(categories);
+//   setLocalStorage(dbCategory);
+//   //code akki
+//   //const data = new formdata(form)
+// };
 
-const deleteCategory = (id) => {
-  const dbCategory = getLocalStorage();
-  const newDbCategory = dbCategory.filter((item) => id !== item.id);
-  setLocalStorage(newDbCategory);
-  window.location.reload();
-};
+// const isValidField = () => {
+//   return document.getElementById("inputsCategories").reportValidity();
+// };
 
-const updateCategory = (index, categories) => {
-  const dbCategory = getLocalStorage();
-  dbCategory[index] = categories;
-  setLocalStorage(dbCategory);
-};
+// const clearFields = () => {
+//   const fields = document.getElementById("nameCategories", "taxCategories");
+//   fields.foreEach((field) => (field.value = ""));
+// };
+// //interação com layout
+// const saveCategory = () => {
+//   if (isValidField()) {
+//     const categories = {
+//       id: Math.random().toString(36).substr(2, 6),
+//       nameCategories: document.getElementById("nameCategories").value.replace(/</g,
+//       "&lt;"
+//       ).replace(/>/g,
+//       "&gt;"
+//       ),
+//       taxCategories: document.getElementById("taxCategories").value.replace(/</g,
+//       "&lt;"
+//       ).replace(/>/g,
+//       "&gt;"
+//       ),
+//     };
+//     createCategory(categories);
+//   }
+// };
 
-updateTable();
+// const createRow = (categories) => {
+//   document.getElementById("tableCategories").appendChild(newRow);
+// };
+
+// const clearTable = () => {
+//   const rows = document.querySelectorAll("#tableCategories>tbody tr");
+//   rows.forEach((row) => row.parentNode.removeChild(row));
+// };
+
+// const updateTable = () => {
+//   const dbCategory = getLocalStorage();
+//   clearTable();
+//   dbCategory.forEach(createRow);
+// };
+
+// const fillFields = (categories) => {
+//   document.getElementById("nameCategories").value = categories.nameCategories;
+//   document.getElementById("taxCategories").value = categories.taxCategories;
+//   document.getElementById("nameCategories").dataset.index = categories.index;
+// };
+
+// const deleteCategory = (id) => {
+//   const dbCategory = getLocalStorage();
+//   const newDbCategory = dbCategory.filter((item) => id !== item.id);
+//   setLocalStorage(newDbCategory);
+//   window.location.reload();
+// };
+
+// const updateCategory = (index, categories) => {
+//   const dbCategory = getLocalStorage();
+//   dbCategory[index] = categories;
+//   setLocalStorage(dbCategory);
+// };
+
+// updateTable();
--- js/products.js
+++ js/products.js
@@ -1,108 +1,167 @@
-const select = document.getElementById("categoryProducts");
+const apiUrl = "http://localhost/routes/products.php";
 
-const getProducts = () => JSON.parse(localStorage.getItem("db_product")) ?? [];
-const setProducts = (dbProduct) =>
-  localStorage.setItem("db_product", JSON.stringify(dbProduct));
+const contactForm = document.getElementById("inputsProducts");
+const contactTable = document.getElementById("tbodyProducts");
 
-const getCategories = () =>
-  JSON.parse(localStorage.getItem("db_categories")) ?? [];
-
-// const categorias = getCategories();
-
-const createRow = (product) => {
-  const newRow = document.createElement("tr");
-  newRow.innerHTML = `
-  <td>${product.id}</td>
-  <td>${product.productName}</td>
-  <td>${product.amount}</td>
-  <td>${product.price}</td>
-  <td>${product.category.nameCategories}</td>
-  <td><button onclick="deleteProduct('${product.id}')">Delete</td>
-  `;
-  document.querySelector("#tableProducts>tbody").appendChild(newRow);
-  console.log(product.category.nameCategories);
-};
-
-const isValidField = () => {
-  return document.getElementById("inputsProducts").reportValidity();
-};
-
-const handleClickAddProduct = () => {
-  if (isValidField()) {
-    const allCategories = getCategories();
-
-    const categoryId = document.getElementById("categoryProducts").value;
-
-    const category = allCategories.find(
-      (category) => category.id === categoryId
-    );
-
-    const product = {
-      id: Math.random().toString(36).substr(2, 9),
-      productName: document
-        .getElementById("nameProductsProd")
-        .value.replace(/</g, "&lt;")
-        .replace(/>/g, "&gt;"),
-      amount: document
-        .getElementById("amountProducts")
-        .value.replace(/</g, "&lt;")
-        .replace(/>/g, "&gt;"),
-      price: document
-        .getElementById("unitPriceProducts")
-        .value.replace(/</g, "&lt;")
-        .replace(/>/g, "&gt;"),
-
-      category: category,
-    };
-
-    console.log(product);
-
-    const products = getProducts();
-    products.push(product);
-    setProducts(products);
-
-    clearFields();
-    updateTable();
-  }
-};
-
-const deleteProduct = (id) => {
-  let dbProduct = getProducts();
-  dbProduct = dbProduct.filter((product) => product.id !== id);
-  setProducts(dbProduct);
-  updateTable();
-};
-
-const clearFields = () => {
-  document.getElementById("nameProductsProd").value = "";
-  document.getElementById("amountProducts").value = "";
-  document.getElementById("categoryProducts").value = "";
-  document.getElementById("unitPriceProducts").value = "";
-};
-
-const renderProducts = () => {
-  const dbProduct = getProducts();
-  dbProduct.forEach(createRow);
-};
-
-const categorySelect = () => {
-  const dbCategories = JSON.parse(localStorage.getItem("db_categories")) ?? [];
-  const selectElement = document.getElementById("categoryProducts");
-
-  selectElement.innerHTML = "<option value='#'>select a Category</option>";
-
-  dbCategories.map((category) => {
-    selectElement.innerHTML += `<option value="${category.id}">${category.nameCategories}</option>`;
+function postarCateg() {
+  contactForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const data = new FormData(contactForm);
+    try {
+      const res = await fetch(
+        "http://localhost/routes/products.php",
+        {
+          method: "POST",
+          body: data,
+        },
+        window.location.reload()
+      );
+    } catch (error) {
+      console.log(error.message);
+    }
   });
-};
+}
 
-const updateTable = () => {
-  const tableBody = document.querySelector("#tableProducts>tbody");
-  tableBody.innerHTML = "";
-  renderProducts();
-};
+async function mostrarProd() {
+  const listaProd = document.getElementById("tbodyCategories");
+  const response = await fetch(apiUrl);
+  const productsList = await response.json();
+  listaProd.innerHTML = "";
 
-document.addEventListener("DOMContentLoaded", () => {
-  categorySelect();
-  renderProducts();
-});
+  productsList.forEach((product) => {
+    const tr = document.createElement("tr");
+    tr.innerHTML = `<tr>
+      <td>${product.code}</td>
+      <td>${product.name}</td>
+      <td>${product.tax}</td>
+      <td><button onclick="deleteCategory(${categories.code})">Delete</td>
+      </tr>`;
+    listaProd.appendChild(tr);
+  });
+}
+
+mostrarProd();
+
+async function showAddedProd() {
+  const response = await fetch(apiUrl);
+  const productsList = await response.json();
+  productsList.forEach((product) => {
+    const tr = document.createElement("tr");
+    tr.innerHTML = `<tr>
+      <td>${product.code}</td>
+      <td>${product.name}</td>
+      <td>${product.tax}</td>
+      <td><button onclick="deleteCategory(${product.code})">Delete</td>
+      </tr>`;
+    listaProd.appendChild(tr);
+  });
+}
+
+// const select = document.getElementById("categoryProducts");
+
+// const getProducts = () => JSON.parse(localStorage.getItem("db_product")) ?? [];
+// const setProducts = (dbProduct) =>
+//   localStorage.setItem("db_product", JSON.stringify(dbProduct));
+
+// const getCategories = () =>
+//   JSON.parse(localStorage.getItem("db_categories")) ?? [];
+
+// // const categorias = getCategories();
+
+// const createRow = (product) => {
+//   const newRow = document.createElement("tr");
+//   newRow.innerHTML = `
+//   <td>${product.id}</td>
+//   <td>${product.productName}</td>
+//   <td>${product.amount}</td>
+//   <td>${product.price}</td>
+//   <td>${product.category.nameCategories}</td>
+//   <td><button onclick="deleteProduct('${product.id}')">Delete</td>
+//   `;
+//   document.querySelector("#tableProducts>tbody").appendChild(newRow);
+//   console.log(product.category.nameCategories);
+// };
+
+// const isValidField = () => {
+//   return document.getElementById("inputsProducts").reportValidity();
+// };
+
+// const handleClickAddProduct = () => {
+//   if (isValidField()) {
+//     const allCategories = getCategories();
+
+//     const categoryId = document.getElementById("categoryProducts").value;
+
+//     const category = allCategories.find(
+//       (category) => category.id === categoryId
+//     );
+
+//     const product = {
+//       id: Math.random().toString(36).substr(2, 9),
+//       productName: document
+//         .getElementById("nameProductsProd")
+//         .value.replace(/</g, "&lt;")
+//         .replace(/>/g, "&gt;"),
+//       amount: document
+//         .getElementById("amountProducts")
+//         .value.replace(/</g, "&lt;")
+//         .replace(/>/g, "&gt;"),
+//       price: document
+//         .getElementById("unitPriceProducts")
+//         .value.replace(/</g, "&lt;")
+//         .replace(/>/g, "&gt;"),
+
+//       category: category,
+//     };
+
+//     console.log(product);
+
+//     const products = getProducts();
+//     products.push(product);
+//     setProducts(products);
+
+//     clearFields();
+//     updateTable();
+//   }
+// };
+
+// const deleteProduct = (id) => {
+//   let dbProduct = getProducts();
+//   dbProduct = dbProduct.filter((product) => product.id !== id);
+//   setProducts(dbProduct);
+//   updateTable();
+// };
+
+// const clearFields = () => {
+//   document.getElementById("nameProductsProd").value = "";
+//   document.getElementById("amountProducts").value = "";
+//   document.getElementById("categoryProducts").value = "";
+//   document.getElementById("unitPriceProducts").value = "";
+// };
+
+// const renderProducts = () => {
+//   const dbProduct = getProducts();
+//   dbProduct.forEach(createRow);
+// };
+
+// const categorySelect = () => {
+//   const dbCategories = JSON.parse(localStorage.getItem("db_categories")) ?? [];
+//   const selectElement = document.getElementById("categoryProducts");
+
+//   selectElement.innerHTML = "<option value='#'>select a Category</option>";
+
+//   dbCategories.map((category) => {
+//     selectElement.innerHTML += `<option value="${category.id}">${category.nameCategories}</option>`;
+//   });
+// };
+
+// const updateTable = () => {
+//   const tableBody = document.querySelector("#tableProducts>tbody");
+//   tableBody.innerHTML = "";
+//   renderProducts();
+// };
+
+// document.addEventListener("DOMContentLoaded", () => {
+//   categorySelect();
+//   renderProducts();
+// });
